cd95264785c612d5cd365e84b3cecd83a9e2cc59
Pavel Bandaronak <p_bandaronak@locport.io>
2022-12-17 21:44:34 +0300

    fix: eventbus typings

--- src/scripts/helpers/EventBus.ts
+++ src/scripts/helpers/EventBus.ts
@@ -1,16 +1,18 @@
+import { eventCallback, eventsDictionary } from '../../types/helpers/IEventBus';
+
 class EventBus {
-  private readonly events: { [key: string]: Array<Function> };
+  private readonly events: eventsDictionary;
 
   constructor() {
     this.events = {};
   }
 
-  on(name: string, func: Function): void {
+  on(name: string, func: eventCallback): void {
     this.events[name] = this.events[name] || [];
     this.events[name].push(func);
   }
 
-  off(name: string, func: Function): void {
+  off(name: string, func: eventCallback): void {
     // const idx = this.events[name].indexOf(func);
     // this.events[name] = this.events[name].splice(idx, 1);
     if (name in this.events) {
@@ -18,7 +20,7 @@ class EventBus {
     }
   }
 
-  trigger(name: string, params?: unknown) {
+  trigger<T>(name: string, params?: T): void {
     if (name in this.events) {
       for (const func of this.events[name]) {
         func(params);
