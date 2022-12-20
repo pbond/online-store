cc30b64e861f89d3a8cc392f499c27ba50f55d97
Pavel Bandaronak <p_bandaronak@locport.io>
2022-12-20 19:12:37 +0300

    feat: remake routes

--- src/scripts/router/Routes.ts
+++ src/scripts/router/Routes.ts
@@ -1,20 +1,19 @@
-import {IRoute} from "../../types/router/IRoute";
+import { IRoute } from '../../types/router/IRoute';
+import { Main } from '../pages/main/Main';
+import { Details } from '../pages/details/Details';
+import { Cart } from '../pages/cart/Cart';
 
 export const Routes: IRoute[] = [
   {
-    path: '#/',
-    component: 'Main'
+    path: '#/main',
+    getPageComponent: (path?: string) => new Main(path),
   },
   {
     path: '#/details',
-    component: 'Details'
+    getPageComponent: (path?: string) => new Details(path),
   },
   {
     path: '#/cart',
-    component: 'Cart'
+    getPageComponent: (path?: string) => new Cart(path),
   },
-  {
-    path: '#/404',
-    component: 'NotFound'
-  }
-]
+];
