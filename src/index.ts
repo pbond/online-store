98bab8076d34d675de6de893ede965893e1366cc
AzureAD\IharAntonenka <i_antonenka@locport.io>
2022-12-21 03:46:59 +0300

    refactor: refactor router

--- src/index.ts
+++ src/index.ts
@@ -2,31 +2,11 @@ import './index.scss';
 import './bootstrap/bootstrap.ts';
 import { IRouter } from './types/router/IRouter';
 import router from './scripts/router/Router';
-import eventBus from './scripts/helpers/EventBus';
-import { Page } from './types/pages/Page';
-import { NotFound } from './scripts/pages/notFound/NotFound';
 
 class Application {
-  protected router: IRouter;
-  protected eventBus;
-  protected rootElement: HTMLElement;
-  protected currentPage: Page;
+  private router: IRouter;
   constructor() {
     this.router = router;
-    this.eventBus = eventBus;
-    this.rootElement = document.body;
-    this.currentPage = new NotFound();
-  }
-
-  init() {
-    this.renderChild = this.renderChild.bind(this);
-    eventBus.on('changePage', this.renderChild);
-  }
-
-  renderChild(page: Page): void {
-    this.currentPage.destroy();
-    this.currentPage = page;
-    this.rootElement.append(page.render());
   }
 
   run(): void {
@@ -36,5 +16,4 @@ class Application {
 }
 
 const app = new Application();
-app.init();
 app.run();
--- src/types/pages/Page.ts
+++ src/types/pages/Page.ts
@@ -1,6 +1,7 @@
 export abstract class Page {
   protected element: HTMLElement;
   protected path: string;
+
   protected constructor(path?: string) {
     this.element = this.render();
     this.path = path ?? '';
