8ae44835c3abb4d3e1894663e693d75016e78eec
AzureAD\IharAntonenka <i_antonenka@locport.io>
2023-01-03 03:27:49 +0300

    feat: add first version of card

--- src/scripts/components/cardInDetails/CardDetails.ts
+++ src/scripts/components/cardInDetails/CardDetails.ts
@@ -1,37 +1,30 @@
 import { Component } from '../../../types/templates/Component';
 import { ElementGenerator } from '../../helpers/ElementGenerator';
 import state from '../../state/State';
-import './vitrineCard.scss';
+import './cardDetails.scss';
 import { IProduct } from '../../../types/models/IProduct';
 import eventBus from '../../helpers/EventBus';
-import { ViewModeEnum } from '../../../types/enums/ViewModeEnum';
 import { Button } from '../button/Button';
 
-export class VitrineCard extends Component {
+export class CardDetails extends Component {
   private product: IProduct;
-  private ViewMode: ViewModeEnum;
   private isInCart: boolean;
-  constructor(tagName: string, className: string, product: IProduct, viewMode: ViewModeEnum) {
+  private imageSizeList: number[];
+  constructor(tagName: string, className: string, product: IProduct) {
     super(tagName, className);
     this.product = product;
-    this.ViewMode = viewMode;
     this.isInCart = false;
+    this.imageSizeList = [];
   }
 
   render(): HTMLElement {
-    switch (this.ViewMode) {
-      case ViewModeEnum.Grid:
-        this.container = this.renderCardGrid();
-        break;
-      case ViewModeEnum.List:
-        this.container = this.renderCardList();
-        break;
-    }
+    this.container.append(this.renderCardList());
+    this.container.append(this.renderCardImages());
     this.init();
     return this.container;
   }
 
-  init(): VitrineCard {
+  init(): CardDetails {
     eventBus.on('addProductToCart', this.addProductToCartHandler.bind(this));
     eventBus.on('removeProductFromCart', this.removeProductFromCartHandler.bind(this));
     return this;
@@ -61,49 +54,64 @@ export class VitrineCard extends Component {
     }
   }
 
-  private renderCardGrid(): HTMLElement {
+  private renderCardList(): HTMLElement {
     this.elements.buttonElement = this.getCartButtonElement();
-    const cardPriceCont = ElementGenerator.createElementByInnerHtml(`
+    const cardPriceCont = ElementGenerator.createElementByInnerHtml<HTMLDivElement>(`
     <div class="card__price d-flex justify-content-end align-items-center">
       <span class="card__price-amount card-text"><b>$${this.product.price}</b></span>
     </div>`);
-    const card = ElementGenerator.createElementByInnerHtml(`
-    <div class="card card-vitrine card-grid mb-3">
-      <div class="row flex-column g-0">
-        <div class="col card__img-cont">
-          <a href="#/details?id=${this.product.id}" class="card__link">
-            <img src="${this.product.thumbnail}" class="card__image card-img-top" alt="${this.product.title}">
-          </a>
-        </div>
-        <div class="col">
-          <div class="card-body">
-            <h5 class="card-title">${this.product.title}</h5>
-            <p class="card__description card-text">${this.product.description}</p>
-            <div class="card__info-container">
-              <div class="card__info">
-                <p class="card__info-item card-text"><small class="text-muted">Catygory: ${this.product.category}</small></p>
-                <p class="card__info-item card-text"><small class="text-muted">Brand: ${this.product.brand}</small></p>
-                <p class="card__info-item card-text"><small class="text-muted">Stock: ${this.product.stock}</small></p>
-                <p class="card__info-item card-text"><small class="text-muted">Rating: ${this.product.rating}</small></p>
-              </div>
-            </div>
+    const card = ElementGenerator.createCustomElement<HTMLDivElement>('div', {
+      className: 'card details__card-cont',
+    });
+    const row = ElementGenerator.createCustomElement<HTMLDivElement>('div', {
+      className: 'row g-0',
+    });
+    const cardImgCont = ElementGenerator.createCustomElement<HTMLDivElement>('div', {
+      className: 'col-sm-4 card__img-cont',
+    });
+    const cardLink = ElementGenerator.createCustomElement<HTMLLinkElement>('a', {
+      className: 'card__link',
+      href: `#/details?id=${this.product.id}`,
+    });
+    this.elements.mainImage = ElementGenerator.createCustomElement<HTMLImageElement>('img', {
+      className: 'card-list__image card-img-top',
+      src: `${this.product.thumbnail}`,
+      alt: `${this.product.title}`,
+    });
+    const cardinfo = ElementGenerator.createElementByInnerHtml<HTMLDivElement>(`
+    <div class="col-sm-8">
+      <div class="card-body">
+        <h5 class="card-title">${this.product.title}</h5>
+        <p class="card__description card-text">${this.product.description}</p>
+        <div class="card__info-container">
+          <div class="card__info">
+            <p class="card__info-item card-text"><small class="text-muted">Catygory: ${this.product.category}</small></p>
+            <p class="card__info-item card-text"><small class="text-muted">Brand: ${this.product.brand}</small></p>
+            <p class="card__info-item card-text"><small class="text-muted">Stock: ${this.product.stock}</small></p>
+            <p class="card__info-item card-text"><small class="text-muted">Rating: ${this.product.rating}</small></p>
+            <p class="card__info-item card-text"><small class="text-muted">Discount: ${this.product.discountPercentage}%</small></p>
           </div>
         </div>
       </div>
     </div>`);
+    cardLink.append(this.elements.mainImage);
+    cardImgCont.append(cardLink);
+    row.append(cardImgCont);
+    row.append(cardinfo);
+    card.append(row);
     cardPriceCont.append(this.elements.buttonElement);
     card.append(cardPriceCont);
     return card;
   }
 
-  private renderCardList(): HTMLElement {
+  private renderCardList2(): HTMLElement {
     this.elements.buttonElement = this.getCartButtonElement();
     const cardPriceCont = ElementGenerator.createElementByInnerHtml(`
     <div class="card__price d-flex justify-content-end align-items-center">
       <span class="card__price-amount card-text"><b>$${this.product.price}</b></span>
     </div>`);
     const card = ElementGenerator.createElementByInnerHtml(`
-    <div class="card card-vitrine">
+    <div class="card details__card-cont">
       <div class="row g-0">
         <div class="col-sm-4 card__img-cont">
           <a href="#/details?id=${this.product.id}" class="card__link">
@@ -120,6 +128,7 @@ export class VitrineCard extends Component {
                 <p class="card__info-item card-text"><small class="text-muted">Brand: ${this.product.brand}</small></p>
                 <p class="card__info-item card-text"><small class="text-muted">Stock: ${this.product.stock}</small></p>
                 <p class="card__info-item card-text"><small class="text-muted">Rating: ${this.product.rating}</small></p>
+                <p class="card__info-item card-text"><small class="text-muted">Discount: ${this.product.discountPercentage}%</small></p>
               </div>
             </div>
           </div>
@@ -129,6 +138,32 @@ export class VitrineCard extends Component {
     cardPriceCont.append(this.elements.buttonElement);
     card.append(cardPriceCont);
     return card;
+  }
+
+  private renderCardImages(): HTMLElement {
+    const card = ElementGenerator.createCustomElement<HTMLDivElement>('div', {
+      className: 'details__images',
+    });
+    this.product.images.forEach(async (img) => {
+      const image = ElementGenerator.createCustomElement<HTMLImageElement>('img', {
+        className: 'card-list__image_small',
+        src: img,
+      });
+      const fileImg = await fetch(img).then((r) => r.blob());
+      if (!this.imageSizeList.some((size) => size === fileImg.size)) {
+        this.imageSizeList.push(fileImg.size);
+        image.addEventListener('click', this.imageClickHandler.bind(this));
+        card.append(image);
+      }
+    });
+    return card;
+  }
+
+  private imageClickHandler(event: MouseEvent): void {
+    const target = event.target;
+    if (target instanceof HTMLImageElement && this.elements.mainImage instanceof HTMLImageElement) {
+      this.elements.mainImage.src = target.src;
+    }
   }
 
   private getCartButtonElement(): HTMLElement {
@@ -146,8 +181,8 @@ export class VitrineCard extends Component {
   }
 
   private changeCartStateHandler(event: MouseEvent): void {
-    const currentTarget = event.target;
-    if (currentTarget instanceof HTMLButtonElement) {
+    const target = event.target;
+    if (target instanceof HTMLButtonElement) {
       if (this.isInCart) {
         eventBus.trigger('removeProductFromCart', this.product);
       } else {
--- src/scripts/pages/details/Details.ts
+++ src/scripts/pages/details/Details.ts
@@ -1,17 +1,50 @@
 import { Page } from '../../../types/templates/Page';
+import './details.scss';
+import { CardDetails } from '../../components/cardInDetails/CardDetails';
+import { IProduct } from '../../../types/models/IProduct';
+import state from '../../state/State';
+import { ElementGenerator } from '../../helpers/ElementGenerator';
 
 export class Details extends Page {
+  private cardDetails: CardDetails | null;
+  private product: IProduct | null;
+
   constructor(path?: string) {
-    super();
-    this.path = path ?? '';
+    super(path);
+    this.product = this.getProductByPath(path);
+    this.cardDetails = null;
+    if (this.product) {
+      this.cardDetails = new CardDetails('section', 'details__card', this.product);
+    }
   }
 
   render(): HTMLElement {
-    this.container.innerHTML = 'Details page';
+    if (this.cardDetails) {
+      this.container.append(this.cardDetails.render());
+    } else {
+      const notFoundElement = ElementGenerator.createCustomElement('h3', {
+        className: 'details__notfound',
+        textContent: 'No products found',
+      });
+      this.container.append(notFoundElement);
+    }
+    this.container.classList.add('details');
     return this.container;
   }
 
-  destroy() {
-    this.container.remove();
+  private getProductByPath(path?: string): IProduct | null {
+    if (!path) {
+      return null;
+    }
+    const pathParams = new URLSearchParams(path);
+    const id = pathParams.get('id');
+    if (!id) {
+      return null;
+    }
+    const product = state.products.find((prod) => prod.id.toString() === id);
+    if (!product) {
+      return null;
+    }
+    return product;
   }
 }
