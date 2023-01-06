02f715cbc3c91021aedfe78d62b8543b920eee45
Pavel Bandaronak <p_bandaronak@locport.io>
2023-01-06 17:21:41 +0300

    feat: add single operations and storage listener

--- src/scripts/state/CartState.ts
+++ src/scripts/state/CartState.ts
@@ -17,39 +17,62 @@ class CartState implements ICartState {
     this.addToCart = this.addToCart.bind(this);
     this.removeFromCart = this.removeFromCart.bind(this);
     this.saveCart = this.saveCart.bind(this);
+    this.addItemToCart = this.addItemToCart.bind(this);
+    this.removeItemFromCart = this.removeItemFromCart.bind(this);
+    this.onStorageUpdated = this.onStorageUpdated.bind(this);
+
     eventBus.on('addProductToCart', this.addToCart);
     eventBus.on('removeProductFromCart', this.removeFromCart);
+    eventBus.on('addProductItem', this.addItemToCart);
+    eventBus.on('removeProductItem', this.removeItemFromCart);
     eventBus.on('cartUpdated', this.saveCart);
+
+    window.addEventListener('storage', this.onStorageUpdated);
   }
 
-  addToCart(product: IProduct): void {
+  addItemToCart(product: IProduct): void {
     const prod = this.products.find((item) => item.product.id === product.id);
     if (prod) {
       prod.count += 1;
-    } else {
-      this.products.push({
-        product,
-        count: 1,
-      });
+      eventBus.trigger('cartUpdated', this.products);
     }
+  }
+
+  removeItemFromCart(product: IProduct): void {
+    const prod = this.products.find((item) => item.product.id === product.id);
+    if (prod) {
+      prod.count -= 1;
+      if (prod.count === 0) {
+        this.products.splice(this.products.indexOf(prod), 1);
+      }
+      eventBus.trigger('cartUpdated', this.products);
+    }
+  }
+
+  addToCart(product: IProduct): void {
+    this.products.push({ product, count: 1 });
     eventBus.trigger('cartUpdated', this.products);
   }
 
   removeFromCart(product: IProduct): void {
     const prod = this.products.find((item) => item.product.id === product.id);
     if (prod) {
-      prod.count -= 1;
-      if (prod.count === 0) {
-        this.products = this.products.splice(this.products.indexOf(prod), 1);
-      }
-    } else {
-      console.error("Can't remove product ", product);
+      this.products.splice(this.products.indexOf(prod), 1);
+      eventBus.trigger('cartUpdated', this.products);
     }
-    eventBus.trigger('cartUpdated', this.products);
   }
 
   saveCart(): void {
+    window.removeEventListener('storage', this.onStorageUpdated);
     localStorage.setItem('cartProducts', JSON.stringify(this.products));
+    window.addEventListener('storage', this.onStorageUpdated);
+  }
+
+  onStorageUpdated(event: StorageEvent) {
+    if (event.newValue !== null) {
+      this.products = JSON.parse(event.newValue);
+      eventBus.trigger('cartUpdated', this.products);
+    }
   }
 }
 
