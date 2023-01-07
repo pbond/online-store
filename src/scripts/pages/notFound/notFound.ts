520344ef0215bbbe56bf823f5a2baa633b3ce279
AzureAD\IharAntonenka <i_antonenka@locport.io>
2023-01-07 13:57:25 +0300

    feat: add 404 page

--- src/scripts/pages/notFound/notFound.ts
+++ src/scripts/pages/notFound/notFound.ts
@@ -1,4 +1,6 @@
 import { Page } from '../../../types/templates/Page';
+import { ElementGenerator } from '../../helpers/ElementGenerator';
+import './notFound.scss';
 
 export class NotFound extends Page {
   constructor(path?: string) {
@@ -7,11 +9,13 @@ export class NotFound extends Page {
   }
 
   render(): HTMLElement {
-    this.container.innerHTML = 'NotFound page';
+    const content = ElementGenerator.createCustomElement<HTMLElement>('h2', {
+      className: 'notfound__content',
+      innerText: '404 Page NOT found',
+    });
+    this.container.append(content);
+    this.container.classList.add('notfound');
+    this.container.classList.add('container');
     return this.container;
-  }
-
-  destroy() {
-    this.container.remove();
   }
 }
